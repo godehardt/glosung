364da889483e3978f7b5ebbb732e2f9c01970db0
Eicke Godehardt <godehardt@fb2.fra-uas.de>
2024-02-21 16:25:58 +0100

    move from g_conf to GSettings

--- src/settings.c
+++ src/settings.c
@@ -20,92 +20,83 @@
 #define PACKAGE "glosung"
 
 #include "settings.h"
-#ifdef WIN32
-	#include "gconf-client.h"
-#else
-	#include <gconf/gconf-client.h>
-#endif
+#include <gio/gio.h>
 
 
 #if (! defined (WIN32) && ! defined (__APPLE__))
   #define VERSE_LINK 1
 #endif
 
-static GConfClient *client = NULL;
+static GSettings *settings = NULL;
 
-#define INIT_CLIENT() \
-	if (! client) { \
-		client = gconf_client_get_default (); \
+#define INIT_SETTINGS() \
+	if (! settings) { \
+		settings = g_settings_new ("org.godehardt.glosung"); \
 	}
+	// settings = g_settings_new_with_path ("org.godehardt.GLosung", "/home/godehardt/DEVELOP/glosung/");
 
 
 gboolean
 is_proxy_in_use ()
 {
-	INIT_CLIENT ();
-	return gconf_client_get_bool (client, "/apps/" PACKAGE "/use_proxy", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_boolean (settings, "use-proxy");
 } /* use_proxy */
 
 
 void
 set_proxy_in_use (gboolean use_proxy)
 {
-	INIT_CLIENT ();
-	gconf_client_set_bool
-		(client, "/apps/" PACKAGE "/use_proxy", use_proxy, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_boolean (settings, "use-proxy", use_proxy);
 } /* set_proxy_in_use */
 
 
 gchar*
 get_proxy ()
 {
-	INIT_CLIENT ();
-	return gconf_client_get_string
-		(client, "/apps/" PACKAGE "/proxy", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_string (settings, "proxy");
 } /* get_proxy */
 
 
 void
 set_proxy (const gchar *proxy)
 {
-	INIT_CLIENT ();
-	gconf_client_set_string
-		(client, "/apps/" PACKAGE "/proxy", proxy, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_string (settings, "proxy", proxy);
 } /* set_proxy */
 
 
 gchar*
 get_herrnhuter_url ()
 {
-        INIT_CLIENT ();
-        return gconf_client_get_string
-                (client, "/apps/" PACKAGE "/losungen-url", NULL);
+        INIT_SETTINGS ();
+        return g_settings_get_string (settings, "losungen-url");
 } /* get_herrnhuter_url */
 
 
 void
 set_herrnhuter_url (const gchar *url)
 {
-        INIT_CLIENT ();
-        gconf_client_set_string
-                (client, "/apps/" PACKAGE "/losungen-url", url, NULL);
+        INIT_SETTINGS ();
+        g_settings_set_string (settings, "losungen-url", url);
 } /* set_herrnhuter_url */
 
 
 gboolean
 is_hide_warning ()
 {
-	INIT_CLIENT ();
-	return gconf_client_get_bool (client, "/apps/" PACKAGE "/hide_warning", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_boolean (settings, "hide-warning");
 } /* is_hide_warning */
 
 
 void
 set_hide_warning (gboolean hide_warning)
 {
-	INIT_CLIENT ();
-	gconf_client_set_bool
-		(client, "/apps/" PACKAGE "/hide_warning", hide_warning, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_boolean (settings, "hide-warning", hide_warning);
 } /* set_hide_warning */
 
 
@@ -113,9 +104,9 @@ GDateTime*
 get_last_usage ()
 {
     GDateTime *last_time = NULL;
-	INIT_CLIENT ();
-	gchar *last_time_str = gconf_client_get_string
-			(client, "/apps/" PACKAGE "/last_time", NULL);
+	INIT_SETTINGS ();
+	gchar *last_time_str = g_settings_get_string
+			(settings, "last-time-opened");
 	if (last_time_str) {
 		last_time = g_date_time_new_from_iso8601 (last_time_str, NULL);
 	}
@@ -127,10 +118,10 @@ get_last_usage ()
 void
 set_last_usage (const GDateTime *date)
 {
-	INIT_CLIENT ();
+	INIT_SETTINGS ();
 	gchar *time_str = g_date_time_format_iso8601 ((GDateTime *)date);
-	gconf_client_set_string
-			(client, "/apps/" PACKAGE "/last_time", time_str, NULL);
+	g_settings_set_string
+			(settings, "last-time-opened", time_str);
 	g_free (time_str);
 } /* set_last_usage */
 
@@ -138,35 +129,33 @@ set_last_usage (const GDateTime *date)
 gchar*
 get_language ()
 {
-	INIT_CLIENT ();
-	return gconf_client_get_string
-		(client, "/apps/" PACKAGE "/language", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_string (settings, "language");
 } /* get_language */
 
 
 void
 set_language (const gchar *language)
 {
-	INIT_CLIENT ();
-	gconf_client_set_string
-	     (client, "/apps/" PACKAGE "/language", language, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_string (settings, "language", language);
 } /* set_language */
 
 
 gboolean
 is_calender_double_click ()
 {
-	INIT_CLIENT ();
-	return gconf_client_get_bool (client, "/apps/" PACKAGE "/calendar_close_by_double_click", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_boolean (settings, "calendar-close-by-double-click");
 } /* is_calender_double_click */
 
 
 void
 set_calender_double_click (gboolean calendar_close_by_double_click)
 {
-	INIT_CLIENT ();
-	gconf_client_set_bool
-		(client, "/apps/" PACKAGE "/calendar_close_by_double_click", calendar_close_by_double_click, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_boolean
+		(settings, "calendar-close-by-double-click", calendar_close_by_double_click);
 } /* set_calender_double_click */
 
 
@@ -174,8 +163,8 @@ gboolean
 is_link_sword ()
 {
 #ifdef VERSE_LINK
-	INIT_CLIENT ();
-	return gconf_client_get_bool (client, "/apps/" PACKAGE "/link_sword", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_boolean (settings, "link-sword");
 #else
 	return FALSE;
 #endif /* VERSE_LINK */
@@ -186,9 +175,9 @@ void
 set_link_sword (gboolean link_sword)
 {
 #ifdef VERSE_LINK
-	INIT_CLIENT ();
-	gconf_client_set_bool
-		(client, "/apps/" PACKAGE "/link_sword", link_sword, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_boolean
+		(settings, "link-sword", link_sword);
 #endif /* VERSE_LINK */
 } /* set_link_sword */
 
@@ -196,16 +185,14 @@ set_link_sword (gboolean link_sword)
 gchar*
 get_font ()
 {
-	INIT_CLIENT ();
-	return gconf_client_get_string
-		(client, "/apps/" PACKAGE "/font", NULL);
+	INIT_SETTINGS ();
+	return g_settings_get_string (settings, "font");
 } /* get_font */
 
 
 void
 set_font (const gchar *font)
 {
-	INIT_CLIENT ();
-	gconf_client_set_string
-	     (client, "/apps/" PACKAGE "/font", font, NULL);
+	INIT_SETTINGS ();
+	g_settings_set_string (settings, "font", font);
 } /* set_font */
